36f2e4250dcafb08ffb94b0a3cd57194ddce55ca
mmazurekWsb <128227042+mmazurekWsb@users.noreply.github.com>
2023-06-10 03:30:17 +0200

    Add files via upload

---
+++ 02sredniaaryt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+
+        a = inputValue();
+        b = inputValue();
+        c = inputValue();
+        double sredniaArytmetyczna;
+
+        sredniaArytmetyczna = (a + b + c) / 3;
+        Console.WriteLine("Sredniaarytmetyczna wynosi "+ sredniaArytmetyczna);
+        Console.ReadKey();
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+        
+
+    }
+}
